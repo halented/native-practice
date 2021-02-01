4c4bc52fd2c63b85b95580f7c494e99333a05ae4
Hal Dunn <heyhal19@gmail.com>
2021-02-01 11:22:01 -0800

    messing with Button and Text core components

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
 
 export default function App() {
+  const [isHungry, setHungry] = useState(true)
+  const title = isHungry ? "Feed Kitty" : "Let kitty nap"
   return (
     <View style={styles.container}>
       <Text>Vivid</Text>
+      <ScrollView>
+        <Image
+          source={{
+            uri: "https://robohash.org/plinko.png"
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+        <Text>Cat Tinkerbell {isHungry ? "is" : "is not"} hungry.</Text>
+        <Button
+          onPress={() => setHungry(!isHungry)}
+          title={title}
+          accessibilityLabel="It's pressable...how can you resist" />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
