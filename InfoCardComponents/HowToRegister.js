3a9bcee0a3329530ab7b5cdb7835a8c75d0fced1
Hal Dunn <heyhal19@gmail.com>
2021-02-03 12:28:23 -0800

    refactor how to register component

--- InfoCardComponents/HowToRegister.js
+++ InfoCardComponents/HowToRegister.js
@@ -9,14 +9,49 @@ function HowToRegister() {
     const [showSpecific, setShowSpecific] = useState(false)
     const [scenario, setScenario] = useState("")
 
+    const renderBasicInstructions = () => {
+        return (
+            <>
+                <Text>First, find out if you have registered before at
+                    <Text style={{ color: 'rgb(32, 137, 220)' }}
+                            onPress={() => Linking.openURL('https://voterlookup.ohiosos.gov/voterlookup.aspx')}>
+                            {" "}Ohio's Voter Lookup.
+                    </Text>
+                        {" "}If you have, you are set to vote. You will need to locate your correct polling location using the
+                    <Text style={{ color: 'rgb(32, 137, 220)' }}
+                            onPress={() => Linking.openURL('https://www.sos.state.oh.us/elections/voters/toolkit/polling-location/')}>
+                            {" "}Ohio Polling Location Tool.
+                    </Text>
+                </Text>
+                <Text>{" "}</Text>
+                <Card.Divider />
+                <Text>If not, you will need to register. You may begin your registration online at
+                    <Text style={{ color: 'rgb(32, 137, 220)' }}
+                            onPress={() => Linking.openURL('https://olvr.ohiosos.gov/')}>
+                            {" "}Ohio's Registration Portal.
+                    </Text>
+                    {" "}Note that the online portal requires an 
+                    <Text style={{ fontWeight: 'bold' }}>Ohio ID (be that a non-expired Ohio ID card or Driver's License), your full name, date of birth, residential address, and Social Security Number.</Text>
+                </Text>
+                <Text>{" "}</Text>
+                <Card.Divider />
+                <Text style={{ textAlign: 'center' }}>
+                    For resources on specific voter scenarios, please select applicable link below:
+                </Text>
+                <Text style={{ textAlign: 'center' }}>
+                    {buttons()}
+                </Text>
+            </>
+        )
+    }
     const renderSpecificScenarioInstructions = () => {
         switch (scenario) {
             case "license":
-                return <OhioIDCard goBack={goBack}/>
+                return <OhioIDCard goBack={goBack} />
             case "military":
-                return <MilitaryVoter goBack={goBack}/>
+                return <MilitaryVoter goBack={goBack} />
             case "unsheltered":
-                return <UnshelteredVoter goBack={goBack}/>
+                return <UnshelteredVoter goBack={goBack} />
             default:
                 return buttons()
         }
@@ -55,38 +90,11 @@ function HowToRegister() {
     }
     return (
         <ScrollView>
-            <Text>First, find out if you have registered before at
-                <Text style={{ color: 'rgb(32, 137, 220)' }}
-                    onPress={() => Linking.openURL('https://voterlookup.ohiosos.gov/voterlookup.aspx')}>
-                    {" "}Ohio's Voter Lookup.
-                </Text>
-                {" "}If you have, you are set to vote. You will need to locate your correct polling location using the
-                <Text style={{ color: 'rgb(32, 137, 220)' }}
-                    onPress={() => Linking.openURL('https://www.sos.state.oh.us/elections/voters/toolkit/polling-location/')}>
-                    {" "}Ohio Polling Location Tool.
-                </Text>
-            </Text>
-            <Text>{" "}</Text>
-            <Card.Divider />
-            <Text>If not, you will need to register. You may begin your registration online at
-                <Text style={{ color: 'rgb(32, 137, 220)' }}
-                    onPress={() => Linking.openURL('https://olvr.ohiosos.gov/')}>
-                    {" "}Ohio's Registration Portal.
-                </Text>
-                {" "}Note that the online portal requires an <Text style={{ fontWeight: 'bold' }}>Ohio ID (be that a non-expired Ohio ID card or Driver's License), your full name, date of birth, residential address, and Social Security Number.</Text>
-            </Text>
-            <Text>{" "}</Text>
-            <Card.Divider />
-            <Text style={{ textAlign: 'center' }}>
-                For resources on specific voter scenarios, please select applicable link below:
-            </Text>
-            <Text style={{ textAlign: 'center' }}>
-                {showSpecific ?
-                    renderSpecificScenarioInstructions()
-                    :
-                    buttons()
-                }
-            </Text>
+            {showSpecific ?
+                renderSpecificScenarioInstructions()
+                :
+                renderBasicInstructions()
+            }
         </ScrollView>
     )
 }
